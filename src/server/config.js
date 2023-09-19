d6dba28ba7c2258e80f9deb4b69fa6bb89535214
jonathan <jorgejonatthan@gmail.com>
2023-09-19 16:00:25 -0600

    host cambiado a 'localhost'

--- src/server/config.js
+++ src/server/config.js
@@ -2,11 +2,11 @@ import { config } from "dotenv";
 config();
 
 let myConfig = {
-    SECRET_KEY: process.env.SECRET_KEY || "secret_key",
+    SECRET_KEY: process.env.SECRET_KEY || "sEc$_+K3y_",
 };
 
 export let PORT = process.env.PORT || 3000;
-export let DB_HOST = process.env.DB_HOST || "localhost";
+export let DB_HOST = "localhost";
 export let DB_PASS = process.env.DB_PASS || "password";
 export let DB_USER = process.env.DB_USER || "root";
 export let DB_NAME = process.env.DB_NAME || "test_db";
